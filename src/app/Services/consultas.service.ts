d95d3e6fe7c8c64da82812fdbaef962e01badcad
sagarme17 <sagarme.17g@gmail.com>
2022-03-21 17:06:34 -0600

    image to base64

--- src/app/Services/consultas.service.ts
+++ src/app/Services/consultas.service.ts
@@ -8,12 +8,18 @@ import { map } from 'rxjs';
 export class ConsultasService {
 
   //El puerto puede cambiar
-  baseURL:string = "http://localhost:8085/PlataformaEducativa/";
+  baseURL:string = "http://localhost:80/";
 
   constructor(private http: HttpClient) { }
 
   Autenticarse(matricula: any,password: any){
     return this.http.post<any>(this.baseURL+"Autenticarse.php",{matricula,password})
+    .pipe(map(User=>{
+      return User;
+    }))
+  }
+  cargarFoto(imagen: any, usuario: any){
+    return this.http.post<any>(this.baseURL+"editarPerfil.php",{imagen,usuario})
     .pipe(map(User=>{
       return User;
     }))
--- src/app/Views/editar-perfil/editar-perfil.component.ts
+++ src/app/Views/editar-perfil/editar-perfil.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../Services/auth/auth.service';
+import { ConsultasService } from 'app/Services/consultas.service';
+import { first, pipe } from 'rxjs';
+
+
+
 
 @Component({
   selector: 'app-editar-perfil',
@@ -6,10 +13,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-perfil.component.scss']
 })
 export class EditarPerfilComponent implements OnInit {
-
-  constructor() { }
+  private image:any;
+  userLogged=this.authService.getUserLogged();
+  constructor(private authService: AuthService,private  _router: Router,private ajax:ConsultasService) { }
 
   ngOnInit(): void {
   }
+  configurar(): void{
+ 
+    this._router.navigateByUrl('/editar-perfil')
+   
+  
+}
+
+cambiarImagen(event:any):void{
+  this.image = <File>event.target.files[0];
+  var reader = new FileReader();
+  reader.readAsDataURL(this.image);
+  reader.onload = function () {
+    console.log('Imageto64',reader.result);
+  };
+  reader.onerror = function (error) {
+    console.log('Error: ', error);
+  };
+
+
+  this.ajax.cargarFoto(reader.result,1);  
+console.log('Image', this.image);
+
+}
+
+
 
 }
