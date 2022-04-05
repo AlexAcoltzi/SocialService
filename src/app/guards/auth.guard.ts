ee9fcf87c0ee4c5def3cc876b08e914654275f8b
Alex Uriel Acoltzi <alex.fantastico22@gmail.com>
2022-04-04 21:35:41 -0500

    Actualizando Login

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.ajax.validator(localStorage.getItem('data'));
+    return this.ajax.validator();
   }
   
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'app/Services/auth/auth.service';
+import { ConsultasService } from 'app/Services/consultas.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -7,15 +9,22 @@ import { AuthService } from 'app/Services/auth/auth.service';
 })
 export class NavBarComponent implements OnInit {
 
+  constructor(private authService: AuthService, private ajax: ConsultasService) { }
 
-  userLogged=this.authService.getUserLogged();
-  constructor(private authService: AuthService) { }
+  userLogged:any;
+
+
+  //name = this.user['nombre'] + " " + this.user['paterno'];
 
   ngOnInit(): void {
+    this.ajax.userLogged.subscribe((valuData)=>{
+      this.userLogged = valuData;
+    }
+    );
   }
   logOut(){
-    localStorage.clear();
-    this.authService.logOut();
+    this.ajax.deleteData();
+    //this.authService.logOut();
   }
 
 }
